ae133cf14da1f356e4d4adfb3c6da5af64975e80
Gabriel Xavier <100116691+0nlySpirit@users.noreply.github.com>
2024-08-05 21:47:34 -0300

    Ruby Symbols

---
+++ symbols/symbols.rb
@@ -0,0 +1,13 @@
+# Backend Developer Ruby
+# by Gabriel Xavier [ Dotket ]
+
+# ----------------- Symbols ------------------
+
+# Allocations of variable in space of memory
+
+puts :name.object_id
+
+# here we set two same variable, and was set allocate in two diferent spaces of memory
+
+puts "name".object_id
+puts "name".object_id
